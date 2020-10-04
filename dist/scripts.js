fecf36bf42e7c6c1443f92fe7ad57e742a9f1a65
hokrisztina <72090744+hokrisztina@users.noreply.github.com>
2020-10-04 12:07:22 +0200

    last exercise for jquery

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,4 +1,19 @@
-$('#First').text('Macrotis');
-$('#Second').css("border-right", "solid 10px purple");
-$('p').css("background", "yellow")
-//exercise 1 from Jquery
+let color = 'purple';
+let number = 10;
+let word = 'Cool';
+if(color === 'purple');{
+    $(".first").css('background', 'purple')
+}
+if (number > 100) {
+    $(".second").text("whoah, that's a big number.");
+} 
+else {
+ $(".second").text("just a regular number, please.");
+    }
+
+if (word='Cool'){
+$(".third").text("Power of DOM");
+}
+else {
+    $(".fourth").text("Power of DOM");
+}
